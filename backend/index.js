6d5b335f1da7e4129c0b52242ec81828ad66e491
Pete <cojocaru.petru12@gmail.com>
2024-08-03 23:28:05 +0300

    merged the front with the back

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const app = express()
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body '), express.json(), cors());
+app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body '), express.json(), express.static('dist'), cors());
 let persons = [
     { 
       "id": "1",
